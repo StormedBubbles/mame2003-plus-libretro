db2749bf644a968f3040454153a53b2dc8537968
mahoneyt944 <49591133+mahoneyt944@users.noreply.github.com>
2022-06-25 15:43:04 -0400

    Update system16.c

--- src/drivers/system16.c
+++ src/drivers/system16.c
@@ -833,7 +833,7 @@ static MACHINE_INIT( afighter ){
 	sys16_fgxoffset = sys16_bgxoffset = 8;
 	sys16_bg_priority_mode=1;
 	sys16_fg_priority_mode=3;
-	sys16_fg_priority_value=0x4000;
+	sys16_fg_priority_value=0x2ff0;
 
 	sys16_tilebank_switch=0x2000; // fixes bg tiles
 
